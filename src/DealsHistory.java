99521d52828aecdec74c2399259215e28ce8e295
nadis-test <nadis.letters@gmail.com>
2022-04-14 17:25:10 +0300

    arrays were changed to lists

--- src/DealsHistory.java
+++ src/DealsHistory.java
@@ -1,30 +1,32 @@
 import sun.jvm.hotspot.utilities.AssertionFailure;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 public class DealsHistory {
     DealsHistory(int deals_container_size){
-        dealsContainer = new BaseDeal[deals_container_size];
+        dealsContainer = new ArrayList<BaseDeal>(deals_container_size);
     }
 
     DealsHistory(){
-        dealsContainer = new BaseDeal[DEALS_CONTAINER_SIZE];
+        dealsContainer = new ArrayList<BaseDeal>(DEALS_CONTAINER_SIZE);
     }
 
     private static final int DEALS_CONTAINER_SIZE = 3;
-    private BaseDeal[] dealsContainer;
+    private List<BaseDeal> dealsContainer;
 
 
     public BaseDeal getDealByIndex(int index){
-        return dealsContainer[index];
+        return dealsContainer.get(index);
     }
 
     public BaseDeal getDealByUUID(String uuid){
         int index = 0;
-        while ((index < dealsContainer.length) && Objects.nonNull(dealsContainer[index]) && !dealsContainer[index].uuid.equals(uuid))
+        while ((index < dealsContainer.size()) && Objects.nonNull(dealsContainer.get(index)) && !dealsContainer.get(index).uuid.equals(uuid))
             index++;
-        if (index < dealsContainer.length) return dealsContainer[index];
+        if (index < dealsContainer.size()) return dealsContainer.get(index);
         else return null;
     }
 
@@ -32,9 +34,9 @@ public class DealsHistory {
 
         int c = 0;
 
-        while ((c < dealsContainer.length) && Objects.nonNull(dealsContainer[c]) && !dealsContainer[c].uuid.equals(deal.uuid))
+        while ((c < dealsContainer.size()) && Objects.nonNull(dealsContainer.get(c)) && !dealsContainer.get(c).uuid.equals(deal.uuid))
             c++;
-        if (c < dealsContainer.length) dealsContainer[c] = deal;
+        if (c < dealsContainer.size()) dealsContainer.set(c, deal);
         else System.out.println("dealsContainer is full");
     }
 
@@ -42,28 +44,20 @@ public class DealsHistory {
         this.deleteDealFromContainer(deal.uuid);
     }
 
-    public void deleteDealFromContainer(String uuid){
-        int c = 0;
-        boolean isDealFound = false;
+    public boolean deleteDealFromContainer(String uuid){
 
-        while ((c < dealsContainer.length) && !isDealFound && (dealsContainer[c] != null)){
-            if (dealsContainer[c].uuid.equals(uuid)) //== сравнивает нам ссылки, а uuid это строка, а не значение
+        for (int c =0; c < dealsContainer.size(); c++) {
+            if (dealsContainer.get(c).uuid.equals(uuid)) //== сравнивает нам ссылки, а uuid это строка, а не значение
             {
-                dealsContainer[c] = null;
-                isDealFound = true;
-                while ((c < dealsContainer.length - 1) && (dealsContainer[c + 1] != null)) {
-                    dealsContainer[c] = dealsContainer[c + 1];
-                    c++;
-                }
+                dealsContainer.remove(c);
+                return true;
             }
-            c++;
         }
-
-        if (!isDealFound) System.out.println("dealsContainer doesn't contain specified deal");
+        return false;
     }
 
     public int getCapacity(){
-        return dealsContainer.length;
+        return dealsContainer.size();
     }
 
 }
